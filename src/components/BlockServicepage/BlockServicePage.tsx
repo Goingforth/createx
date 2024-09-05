183bcf9c3ec75209c2b2e543883b0ad2fecab6e5
Oleksandr Siukov <as-service2008@ukr.net>
2024-09-05 21:32:23 +0300

    services page

--- src/components/BlockServicepage/BlockServicePage.tsx
+++ src/components/BlockServicepage/BlockServicePage.tsx
@@ -1,24 +1,40 @@
 import { FC } from "react";
+import ButtonBasic from "../../uikit/Buttons/ButtonBasic/ButtonBasic";
 import styles from "./BlockServicePage.module.scss";
 
 type Props = {
   index: number;
+  src: string;
+  title: string;
+  info: string;
 };
 
-const BlockServicePage: FC<Props> = ({ index }) => {
+const BlockServicePage: FC<Props> = ({ index, title, src, info }) => {
   return (
-    <div className={styles.container}>
-      {index % 2 ? (
-        <>
-          <div className={styles.blockInfo}>text</div>
-          <div className={styles.imgWrapper}>img</div>
-        </>
-      ) : (
-        <>
-          <div className={styles.imgWrapper}>img</div>
-          <div className={styles.blockInfo}>text</div>
-        </>
-      )}
+    <div
+      className={styles.container}
+      style={{ flexDirection: index % 2 ? "row-reverse" : "row" }}
+    >
+      <div className={styles.imgWrapper}>
+        <img src={src} alt='' />
+      </div>
+      <div className={styles.blockInfo}>
+        <h2 className={styles.blockInfoTitle}>{title}</h2>
+        <div
+          className={styles.info}
+          dangerouslySetInnerHTML={{
+            __html: `<p>${info}</p>`,
+          }}
+        />
+        <ButtonBasic
+          size='regular'
+          title='Learn more'
+          bg='transparent'
+          titleColor='primary'
+          borderColor='primary'
+          border
+        />
+      </div>
     </div>
   );
 };
--- src/uikit/Buttons/ButtonBasic/ButtonBasic.tsx
+++ src/uikit/Buttons/ButtonBasic/ButtonBasic.tsx
@@ -4,9 +4,11 @@ import styles from "./ButtonBasic.module.scss";
 type Props = {
   type?: "submit" | "reset" | "button";
   title?: string;
+  titleColor?: "white" | "primary";
   bg?: "primary" | "transparent";
   size: "large" | "regular" | "small";
   border?: boolean;
+  borderColor?: "white" | "primary";
   borderR?: "none" | "all" | "left" | "right";
   disabled?: boolean;
   onClick?: any;
@@ -15,9 +17,11 @@ type Props = {
 const ButtonBasic: FC<Props> = ({
   type = "button",
   title = "Large Button",
+  titleColor,
   bg = "primary",
   size,
   border,
+  borderColor,
   borderR,
   disabled,
   onClick,
@@ -36,10 +40,14 @@ const ButtonBasic: FC<Props> = ({
         size === "regular" ? styles.regular : "",
         size === "small" ? styles.small : "",
         border ? styles.border : "",
+        borderColor === "white" ? styles.borderColorWhite : "",
+        borderColor === "primary" ? styles.borderColorPrimary : "",
         borderR === "none" ? styles.borderR_None : "",
         borderR === "all" ? styles.borderR_All : "",
         borderR === "left" ? styles.borderR_Left : "",
         borderR === "right" ? styles.borderR_Right : "",
+        titleColor === "white" ? styles.titleColorWhite : "",
+        titleColor === "primary" ? styles.titleColorPrimary : "",
       ].join(" ")}
     >
       {title}
