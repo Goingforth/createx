02a084675d96fda727b0659b7e492a4238c3797d
Oleksandr Siukov <as-service2008@ukr.net>
2024-09-07 00:01:18 +0300

    work interior

---
+++ src/components/Offer/Offer.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import styles from "./Offer.module.scss";
+
+const Offer: FC = () => {
+  return (
+    <div className={styles.container}>
+      <div>1</div>
+      <div>2</div>
+    </div>
+  );
+};
+
+export default Offer;
--- src/pages/InteriorDesign/InteriorDesign.tsx
+++ src/pages/InteriorDesign/InteriorDesign.tsx
@@ -1,13 +1,16 @@
 import { FC } from "react";
 import HeroPage from "../../components/HeroPage/HeroPage";
+import Offer from "../../components/Offer/Offer";
 import styles from "./InteriorDesign.module.scss";
 
 const InteriorDesign: FC = () => {
   return (
     <>
-      <div>
-        <HeroPage />
+      <HeroPage />
+      <div className={styles.container}>
+        <Offer />
       </div>
+
       {/* <div className={styles.container}>
         {dataServicesPage.map(({ src, title, info, route }, index) => (
           <BlockServicePage
