fde9f33609cb63a73815fb7fb3f8db4ccac8eb22
Oleksandr Siukov <as-service2008@ukr.net>
2024-10-11 15:15:21 +0300

    Map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,11 +1,26 @@
 import { FC } from "react";
 import SectionWithChildren from "../SectionWithChidren/SectionWtithChildren";
+import SvgSpriteCountry from "../../uikit/SvgSpriteCountry/SvgSpriteCountry";
+import { dataCountryFlag } from "../../data";
 import styles from "./Map.module.scss";
 
 const MapImg: FC = () => {
   return (
     <div className={styles.mapWrapper}>
       <img src='/map.jpg' alt='Map' />
+
+      {dataCountryFlag.map((country) => (
+        <div
+          key={country.id}
+          style={{
+            position: "absolute",
+            top: `${country.top}px`,
+            left: `${country.left}px`,
+          }}
+        >
+          <SvgSpriteCountry id={country.icon} />
+        </div>
+      ))}
     </div>
   );
 };
