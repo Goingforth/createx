6ea125d75b08ac3257815564ff0d681824e32b86
Oleksandr Siukov <as-service2008@ukr.net>
2024-10-28 21:21:22 +0200

    error

--- src/components/TestimonialsCard/TestimonialsCard.tsx
+++ src/components/TestimonialsCard/TestimonialsCard.tsx
@@ -4,7 +4,7 @@ import { TypeTestimonialsCards } from "../../data";
 import styles from "./TestimonialsCard.module.scss";
 
 const TestimonialsCard: FC<TypeTestimonialsCards> = (card) => {
-  const { profileImg, image, content, name, position } = card;
+  const { profileImg, content, name, position } = card;
   return (
     <div className={styles.container}>
       <div className={styles.faceWrapper}>
@@ -13,9 +13,6 @@ const TestimonialsCard: FC<TypeTestimonialsCards> = (card) => {
       <div className={styles.content}>{content}</div>
       <div className={styles.name}>{name}</div>
       <div className={styles.position}>{position}</div>
-      {/* <div className={styles.imageWrapper}>
-        <img src={image} alt='' />
-      </div> */}
     </div>
   );
 };
