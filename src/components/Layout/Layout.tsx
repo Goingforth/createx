3163b62c1f006f3efaec8cfa5280a438e77bf97d
Oleksandr Siukov <as-service2008@ukr.net>
2025-03-30 14:51:51 +0300

    insert layout

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,44 @@
+import { FC, lazy, Suspense } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import { RingLoader } from "react-spinners";
+import { Header } from "../index";
+const Footer = lazy(() => import("../Footer/Footer"));
+const Application = lazy(() => import("../Application/Application"));
+import styles from "./Layout.module.scss";
+
+const Loading = () => (
+  <div className={styles.spinner}>
+    <RingLoader
+      color={"grey"}
+      loading={true}
+      size={400}
+      aria-label='Loading Spinner'
+      data-testid='loader'
+    />
+  </div>
+);
+
+const Layout: FC = () => {
+  const location = useLocation();
+  return (
+    <Suspense fallback={<Loading />}>
+      <div className={styles.body}>
+        <div className={styles.container}>
+          <Header />
+        </div>
+        <main>
+          <Suspense fallback={<Loading />}>
+            <Outlet />
+          </Suspense>
+        </main>
+
+        {location.pathname !== "/contacts" && <Application />}
+
+        <footer className={styles.footer}>
+          <Footer />
+        </footer>
+      </div>
+    </Suspense>
+  );
+};
+export default Layout;
