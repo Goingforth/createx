62101a925a332359e2f728150518c968165aae0e
Oleksandr Siukov <as-service2008@ukr.net>
2025-04-04 13:06:45 +0300

    loading wait text

--- src/uikit/LoadingWait/LoadingWait.tsx
+++ src/uikit/LoadingWait/LoadingWait.tsx
@@ -4,7 +4,7 @@ import styles from "./LoadingWait.module.scss";
 const LoadingWait: FC = () => {
   return (
     <div className={styles.container}>
-      <h1>SERVER ERROR</h1>
+      <h1>Please weait loading...</h1>
     </div>
   );
 };
